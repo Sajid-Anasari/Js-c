ced2d38f5034dcd427eedc5b2f2f55b74716d169
Sajid-Anasari <146724412+Sajid-Anasari@users.noreply.github.com>
2023-10-22 04:48:24 +0000

    dataType

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict" //treat all JS code as newer verson
+
+//alert(3+3) //we are using nodesjs,not browser
+
+console.log(3 +3)
+console.log("Sajid")
+
+let name ="Sjaid"
+let age =18
+let isLoggedIn =false
+let stste;
+//number => 2 to power 53
+//bigint
+//string=> true/false
+
+console.log(typeof age);
+
+
+
+
+
